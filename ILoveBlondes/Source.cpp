3881f92cf2c190f9b4521e99433eaf12332f4418
marioandreibobi <bobimarioandrei@gmail.com>
2025-10-23 09:24:03 +0300

    inceput

--- ILoveBlondes/Source.cpp
+++ ILoveBlondes/Source.cpp
@@ -1,16 +1,1 @@
 #include <iostream>
-
-int main() {
-
-	std::cout << "HELLO";
-	std::cout << "Hello";
-
-	std::cout << "ALEX E GAYYYY";
-
-	std::cout << "cel mai gay ?";
-
-	int gay;
-	std::cin >> gay;
-
-	return 0;
-}
