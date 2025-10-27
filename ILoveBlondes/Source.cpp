ceadb6270d67dada0a5a814db2c7d33a7995ae34
marioandreibobi <bobimarioandrei@gmail.com>
2025-10-27 13:25:11 +0200

    first function

--- ILoveBlondes/Source.cpp
+++ ILoveBlondes/Source.cpp
@@ -1,1 +1,31 @@
 #include <iostream>
+#include <cstring>
+
+enum CommandType {
+	CREATE_TABLE,
+	DROP_TABLE,
+	DISPLAY_TABLE,
+	CREATE_INDEX,
+	DROP_INDEX,
+	INSERT,
+	DELETE,
+	SELECT,
+	UPDATE,
+	EXIT,
+	UNKNOWN
+};
+
+CommandType getCommandType(std::string command) {
+	if (command == "CREATE TABLE") return CREATE_TABLE;
+	else if (command == "DROP TABLE") return DROP_TABLE;
+	else if (command == "DISPLAY TABLE") return DISPLAY_TABLE;
+	else if (command == "CREATE INDEX") return CREATE_INDEX;
+	else if (command == "DROP INDEX") return DROP_INDEX;
+	else if (command == "INSERT") return INSERT;
+	else if (command == "DELETE") return DELETE;
+	else if (command == "SELECT") return SELECT;
+	else if (command == "UPDATE") return UPDATE;
+	else if (command == "EXIT")return EXIT;
+	else return UNKNOWN;
+}
+
