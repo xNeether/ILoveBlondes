eb4a58ba0a58e1c8a70143d4558debcfe976ac8b
Costache Filip <filipcostache8@gmail.com>
2025-10-29 02:11:41 +0200

    scheme for interface

--- ILoveBlondes/interface.cpp
+++ ILoveBlondes/interface.cpp
@@ -6,20 +6,93 @@ using namespace std;
 
 void Separator() {
 
-	cout << "------------------------------------" << endl;
+    cout << "------------------------------------" << endl;
+    //sa fie mai pretty saracia am pus si spatiu ca de ce nu.
 
 }
-void CreateTable(){
+
+
+void CreateTable() {
+
 
 }
-void {
+
+
+void DropTable() {
+
 
 }
+
+
+void DisplayTable() {
+
+
+}
+
+
+void CreateIndex() {
+
+
+}
+
+
+void DropIndex() {
+
+
+}
+
+void InsertData() {
+
+
+}
+
+
+
+void SelectData() {
+
+
+}
+
+
+void Update() {
+    //update the data at any given time ca sa fie si omul ala la curent cu schimbarile
+
+}
+
+
+void Delete() {
+
+    //ceva delete command i suppose pentru ca o sa greseasca oamenii
+
+}
+
+
+void Assistance() {
+    cout << "SQL HELP PANEL" << endl;
+    cout << "Supported commands:" << endl;
+
+    //create table,
+    //drop table
+    //display table
+    // create index
+    //drop index
+    //insert values
+    //select columns or whatever idk yet
+    //  select all maybe?
+    //delete a table 
+    //etc.... whatever is needed
+    //
+    //
+    //
+    Separator();
+}
+
 
 void DisplayStatus() {
 
     Separator();
 }
+
 
 int main() {
     cout << "------------------------------------" << endl;
